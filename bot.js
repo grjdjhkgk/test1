0ce78944e9541cdf87c53a7e147d227a7e040337
grjdjhkgk <56803584+grjdjhkgk@users.noreply.github.com>
2019-10-20 23:00:21 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("635581466956398602")
+setInterval(function() {
+channel.send(`jabker jabker jabker jabker jabker`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
